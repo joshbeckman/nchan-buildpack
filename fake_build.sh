13527bf83e6fbe415f6f2518d78b719053a1818b
Josh Beckman <email@andjosh.com>
2017-03-29 14:24:05 -0500

    Keep things under /bin

--- fake_build.sh
+++ fake_build.sh
@@ -10,9 +10,9 @@ echo '====== Making fake dir'
 mkdir -p $fake_path
 touch $fake_path/nchan.conf.erb
 
-echo '====== bin/detect'
-./bin/detect $fake_path
-echo '====== bin/compile'
-./bin/compile $fake_path $fake_path
-echo '====== bin/release'
-./bin/release $fake_path
+echo '====== bin/detect' \
+    && ./bin/detect $fake_path \
+    && echo '====== bin/compile' \
+    && ./bin/compile $fake_path $fake_path \
+    && echo '====== bin/release' \
+    && ./bin/release $fake_path
